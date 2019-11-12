4f7e5e6a4a5fdad416ceffee15f3c500779e5b17
salahuddin <salahuddin128@gmail.com>
2019-11-13 01:14:51 +0500

    ace.js code editor

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
+app = Flask("See")
 
-app = Flask(__name__)
+@app.route('/public/<path:path>')
+def send_public(path):
+    return send_from_directory('public', path)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return render_template('main.html')
 
